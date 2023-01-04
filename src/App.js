044acf621ecc92b0c4f767baa6c848582781ae95
kent <kentvan21@gmail.com>
2023-01-03 19:04:21 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import Grid from "./components/Grid";
+import Cell from "./components/Cell";
+import { BFS } from "./algorithms/BFS";
 
 function App() {
+  // button to visualize depending on selected algorithm?
+  // create a grid
+  // should have functions here?
+
+  let grid = [];
+  const start = [3, 2];
+  const end = [5, 7];
+
+  // animate function - go through shortest path and change grid accordingly
+  // const BFS = () => {
+  //   // need shortest path and visited nodes taken
+  //   console.log("bfs");
+  //   alert("bfs");
+  // };
+
+  for (let row = 0; row < 10; row++) {
+    for (let col = 0; col < 10; col++) {
+      const isStart = row === start[0] && col === start[1];
+      const isEnd = row === end[0] && col === end[1];
+
+      grid.push(
+        <Cell
+          id={`${row}-${col}`}
+          key={[row, col]}
+          isStart={isStart}
+          isEnd={isEnd}
+        />
+      );
+    }
+  }
+
+  // function to send to change into wall via documentGetId?
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello, World!</h1>
+      <p>this some text from paragraph</p>
+      <button onClick={() => BFS(grid, start, end)} className="btn">
+        Visualize
+      </button>
+      {/* <Grid rows={10} columns={10} start={[3, 2]} end={[5, 7]} /> */}
+
+      <div className="gridContainer">{grid}</div>
     </div>
   );
 }
