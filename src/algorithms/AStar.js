d8c45404b269f591c69aeeda0665ea9848ab8fc9
kent <kentvan21@gmail.com>
2023-04-05 21:01:10 -0500

    implemented A* non-weighted

--- src/algorithms/AStar.js
+++ src/algorithms/AStar.js
@@ -5,6 +5,10 @@ let shortestPath = [];
 let found = false;
 
 export function AStar(start, end, rows, cols) {
+  visitedOrder = [];
+  shortestPath = [];
+  found = false;
+
   solve(start, end, rows, cols);
 
   shortestPath[[start[0], start[1]]] = "start";
@@ -19,11 +23,7 @@ export function AStar(start, end, rows, cols) {
 function solve(start, end, rows, cols) {
   let open = []; // contains nodes to be visited. Sorted from lowest to highest f value
   let closed = []; // contains nodes that are visited
-  let fList = [];
-  let parentDist = 0;
-
-  open.push([start[0], start[1]]);
-  fList[start] = Math.abs(start[0] - end[0]) + Math.abs(start[1] - end[1]); // initialize f-value for getLowest()
+  let fList = []; // contains the parent distance and estimated cost from the end
 
   // let test = open.splice(0, 1)[0];
   // let test = open.pop();
@@ -35,9 +35,14 @@ function solve(start, end, rows, cols) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       closed[[i, j]] = false;
-      fList[[i, j]] = Infinity;
+      fList[[i, j]] = [Infinity, Infinity];
     }
   }
+
+  open.push([start[0], start[1]]);
+  fList[[start[0], start[1]]][0] = 0;
+  fList[[start[0], start[1]]][1] =
+    Math.abs(start[0] - end[0]) + Math.abs(start[1] - end[1]); // initialize heuristics for getLowest()
 
   // getLowest() test case - should return the node with lowest f-value
   // open.push([11, 13]);
@@ -85,8 +90,10 @@ function solve(start, end, rows, cols) {
       if (closed[[nRow, nCol]] === true) continue;
 
       // else, compute f value
-      let fValue =
-        parentDist + (Math.abs(nRow - end[0]) + Math.abs(nCol - end[1]));
+      let parentDist = fList[[row, col]][0]; // distance to parent
+      let heuristics = Math.abs(nRow - end[0]) + Math.abs(nCol - end[1]); // estimated cost from end
+
+      let fValue = parentDist + heuristics; // f-value of this neighbor
       // console.log(fValue);
 
       // look through current open list to determine if neighbor is in open
@@ -95,19 +102,21 @@ function solve(start, end, rows, cols) {
         let node = open[i];
         if (node[0] === nRow && node[1] === nCol) {
           inOpen = true;
-          console.log("found node in open list");
+          // console.log("found node in open list");
         }
       }
 
       if (!inOpen) {
         // add to open list and f-value list
         open.push(neighbor);
-        fList[[nRow, nCol]] = fValue;
+        fList[[nRow, nCol]][0] = parentDist;
+        fList[[nRow, nCol]][1] = heuristics;
         shortestPath[[nRow, nCol]] = cur;
       } else {
         // check f-value and adjust f-value and parent if new f-value is lower
-        if (fValue < fList[[nRow, nCol]]) {
-          fList[[nRow, nCol]] = fValue;
+        if (fValue < fList[[nRow, nCol]][0] + fList[[nRow, nCol]][1]) {
+          fList[[nRow, nCol]][0] = parentDist;
+          fList[[nRow, nCol]][1] = heuristics;
           shortestPath[[nRow, nCol]] = cur;
         }
       }
@@ -133,7 +142,11 @@ function getLowest(open, fList) {
   // find the index of the node with lowest f-value
   for (let i = 0; i < open.length; i++) {
     let node = open[i];
-    let newValue = fList[[node[0], node[1]]];
+
+    let parentDist = fList[[node[0], node[1]]][0];
+    let heuristics = fList[[node[0], node[1]]][1];
+
+    let newValue = parentDist + heuristics;
 
     if (newValue < prevValue) {
       prevValue = newValue;
@@ -147,4 +160,19 @@ function getLowest(open, fList) {
   // console.log("lowest node: " + lowestNode);
 
   return lowestNode;
+}
+
+// returns the weight of node
+function nodeWeight(row, col) {
+  let nodeName = document.getElementById(row + "-" + col).className;
+
+  if (nodeName === "cell weight1") {
+    return 1;
+  } else if (nodeName === "cell weight2") {
+    return 2;
+  } else if (nodeName === "cell weight3") {
+    return 3;
+  } else {
+    return 0;
+  }
 }
